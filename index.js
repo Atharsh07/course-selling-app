5696a30a313add398ebadb7ed94e51a3ad603748
atharsh07 <atharsh0425@gmail.com>
2024-10-30 00:27:15 +0530

    routes init

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 const express = require('express');
 const app = express();
+const {userRouter, courseRouter} = require('./routes/user.js')
 
-
-app.post('/user/signup', function (req, res) {
-    res.json({
-        message: 'sigup endpoint'
-    })
-})
-app.post('/user/sigin', function (req, res) {
-    res.json({
-        message: 'sigin endpoint'
-    })
-})
-app.post('/user/buy', function (req, res) {
-    res.json({
-        message: 'buy endpoint'
-    })
-})
-app.get('/course/purchases', function (req,res) {
-    res.json({
-        message: 'purchases endpoint'
-    })
-})
-app.get('/course/preview', function (req, res) {
-    res.json({
-        message: 'courses endpoint'
-    })
-})
-
-
-
+app.use('/users', userRouter);
+app.user('/course', courseRouter)
 app.listen(3000);
