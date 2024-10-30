45718644bad5ce028376d2ee34914ec4af74e720
atharsh07 <atharsh0425@gmail.com>
2024-10-30 12:31:24 +0530

    middleware

--- db.js
+++ db.js
@@ -4,16 +4,27 @@ const ObjectId = mongoose.Types.ObjectId;
 
 
 const userSchema = new Schema({
-    
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
 })
 const adminSchema = new Schema({
-
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
 })
 const courseSchema = new Schema({
-
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
 })
 const purchaseSchema = new Schema({
-
+    userId : ObjectId,
+    courseId : ObjectId
 })
 
 const userModel = mongoose.model('user', userSchema);
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
+console.log(process.env.MONGO_URL)
+const express = require("express");
+const mongoose = require("mongoose");
 const {userRouter} = require('./routes/user.js')
 const {courseRouter} = require('./routes/course.js')
 const {adminRouter} = require('./routes/admin.js')
+const app = express();
+app.use(express.json());
+
 app.use('/users', userRouter);
 app.use('/course', courseRouter);
 app.use('/admin', adminRouter);
-app.listen(3000);
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
+
+main()
